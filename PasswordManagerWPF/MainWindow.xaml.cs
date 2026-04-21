d9357a0dcdb1f906c7003113e3965e953dcfe7e2
David Burykin <Davyd.Burykin@student.uzhnu.edu.ua>
2026-04-22 01:12:18 +0300

    Implemented page navigation with Frame

--- PasswordManagerWPF/MainWindow.xaml.cs
+++ PasswordManagerWPF/MainWindow.xaml.cs
@@ -1,45 +1,37 @@
-﻿using System.Windows;
-using System.Windows.Media;
+﻿using System;
+using System.Windows;
 namespace PasswordManagerWPF
 {
+    ///<summary>
+    ///Логіка взаємодії для MainWindow.xaml
+    ///</summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            //Замість сторінки логіну поки що просто показуємо вітальне повідомлення у Frame
-            MainFrame.Content = new System.Windows.Controls.TextBlock
-            {
-                Text = "Ласкаво просимо до Сейфу!\nОберіть дію в меню ліворуч.",
-                FontSize = 24,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                Foreground = new SolidColorBrush(Colors.DarkGray),
-                TextAlignment = TextAlignment.Center
-            };
+            //Відкриваємо сторінку логіну при запуску програми
+            MainFrame.Navigate(new Views.LoginPage());
         }
 
         //Обробник для кнопки "Мої паролі"
         private void btnShowList_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Тут буде відкриватися список ваших паролів.", "Інформація", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainFrame.Navigate(new Views.PasswordsListPage());
         }
 
         //Обробник для кнопки "Додати новий"
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Тут буде відкриватися форма для додавання нового сайту та пароля.", "Інформація", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainFrame.Navigate(new Views.AddPasswordPage());
         }
 
         //Обробник для кнопки "Вийти"
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Ви впевнені, що хочете закрити сейф?", "Вихід", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes)
-            {
-                Application.Current.Shutdown();
-            }
+            //Повернення на екран блокування (логіну)
+            MainFrame.Navigate(new Views.LoginPage());
         }
     }
 }
